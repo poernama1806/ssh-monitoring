b8179441741d052be84b672ff9f38ae56279c78a
Purnama <herry.purnama@outlook.com>
2022-03-10 18:27:55 +0000

    add file monitoring

--- client/client.py
+++ client/client.py
@@ -1,16 +1,35 @@
-from ipaddress import ip_address
-import re
 import requests
 import socket
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+# The Monitoring server
+SERVER = "http://172.26.10.171:5000/"
 
 # Get the hostname
 hostname = socket.gethostname()
 
-# Get the attempt
-file = open("/var/log/auth.log", "r")
-data = file.read()
-attempt = data.count("authentication failure")
+def on_modified(event):
+    # Get the attempt
+    file = open("/var/log/auth.log", "r")
+    data = file.read()
+    attempt = data.count("authentication failure")
+    requests.put(SERVER + hostname, {"hostname":hostname,"attempt":attempt})
+    print("modified")
 
-SERVER = "http://127.0.0.1:5000/"
+if __name__ == "__main__":
+    path = "/var/log/auth.log"
+    event_handler = FileSystemEventHandler()
+    event_handler.on_modified=on_modified
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    observer.start()
+    try:
+        print("Monitoring")
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
 
-response = requests.put(SERVER + hostname, {"hostname":hostname,"attempt":attempt})
