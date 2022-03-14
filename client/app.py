20a14f9806e8ce1918712ed9c51ddc38c17eea5c
herry <hpgcloud001@gmail.com>
2022-03-14 10:06:29 +0000

    add post and update put path

--- client/app.py
+++ client/app.py
@@ -5,17 +5,18 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
 # The Monitoring server
-SERVER = "http://172.26.10.171:5000/host/"
+SERVER = "http://172.26.10.171:5000/"
 
 # Get the hostname
 hostname = socket.gethostname()
+requests.post(SERVER + "hosts", {"hostname":hostname,"attempt": 0})
 
 def on_modified(event):
     # Get the attempt
     file = open("/var/log/auth.log", "r")
     data = file.read()
     attempt = data.count("authentication failure")
-    requests.put(SERVER + hostname, {"hostname":hostname,"attempt":attempt})
+    requests.put(SERVER + "host/" + hostname, {"hostname":hostname,"attempt":attempt})
     print("modified")
 
 if __name__ == "__main__":
